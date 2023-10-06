724bd19c4ce62fb89224e3b7b830d7866f008a9c
akashkroy69 <akashkroy@hotmail.com>
2023-10-07 04:00:53 +0530

    3rd commit, bird setup done

--- FlapFlap.py
+++ FlapFlap.py
@@ -6,6 +6,12 @@ window.title("Flap Flap")
 window.setup(1000,600)
 window.bgcolor("skyblue")
 
+#bird setup
+bird = turtle.Turtle()
+# bird.shape()
+bird.color("yellow")
+
+
 
 #keeps window open util manually closed
 turtle.done()
